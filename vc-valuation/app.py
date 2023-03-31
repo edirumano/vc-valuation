595b3a3378326f756e776d490156f52f62882c0c
Edi Rumano <123773802+edirumano@users.noreply.github.com>
2023-03-31 19:40:12 -0400

    working waterfall

--- vc-valuation/app.py
+++ vc-valuation/app.py
@@ -1,87 +1,210 @@
 from flask import Flask, render_template, request
+import numpy as np
+import matplotlib.pyplot as plt # for plotting
+import matplotlib.cm as cm #for color mapping
+import time
 
 app = Flask(__name__, static_url_path='/static', static_folder='static')
 
-
-
 class Investor:
-    def __init__(self,investorID,  invested=0):
-        self.investorID = investorID
-        self.invested = invested
-        self.cumdistributed = 0
-    
-class Common:
-    def __init__(self, investorID, ranking=0, shares=0, active=1, distributed=0):
-        self.holder = investorID
-        self.ranking = ranking
-        self.shares = shares
-        self.active = active
-        self.distributed = distributed
-        self.type_name = "Common Stock"
-        
-    
-    def calc_conversion_value(self, company_val, com_securities, pref_securities):
-        if self.active == 1:
-            return 0
-        else:
+    def __init__(self, invID, name, gppercent=0,fees=0,comcap=0):
+        self.invID = invID
+        self.name = name
+        self.gppercent = gppercent
+        self.fees = fees
+        self.comcap = comcap
+        self.invested = 0
+        self.invdistributed = 0
 
-   
-class Preferred:
-    def __init__(self, investorID, ranking, app, liqpref, distributed=0):
-        self.holder = investorID
-        self.ranking = ranking
+        
+
+class Security:
+    type_names = {"com": "Common Stock", "red_pref": "Redeemable Preferred", "part_pref": "Participating Preferred", "conv_pref": "Convertible Preferred"}
+
+    def __init__(self, secID, holder, type="com", comshares=0, app=0, liqpref=0, ranking=1):
+        self.secID = secID
+        self.holder = holder
+        self.type = type
+        self.type_name = Security.type_names[type]
+        self.comshares = comshares
         self.app = app
         self.liqpref = liqpref
-        self.distributed = distributed
+        self.redeemval = self.app * self.liqpref
+        self.ranking = ranking
+        self.secdistributed = 0
+        # Check if the security is convertible and set active to 0 if it is
+        if self.type=="conv_pref":
+            self.active = 0
+            self.rvps = self.redeemval / self.comshares
+        else:
+            self.active = 1
+        self.dumactive = self.active
+
+
+def initialize_investors(form_data, total_investors):
+    # initialize_investors takes the form data and the total number of investors and returns a list of investor objects
     
-
-class InvestorSecurity:
-    type_names = {"red_pref": "Redeemable Preferred", "part_pref": "Participating Preferred", "conv_pref": "Convertible Preferred"}
+    investors = []
     
-    def __init__(self, investorID, common=None, preferred=None, sec_type=None):
-        self.holder = investorID
-        self.common = common
-        self.preferred = preferred
-        self.sec_type = sec_type
-        self.type_name = InvestorSecurity.type_names[sec_type]
-        # self.cap = cap (define later and put in args)
-        # self.QPO = QPO (define later and put in args)
+    for i in range(total_investors+1):
+        # obtain the relevant data
+        name = form_data.get(f'investors[{i}].name', "Investor " + str(i))
+        gppercent = float(form_data.get(f'investors[{i}].gppercent', 0))
+        fees = float(form_data.get(f'investors[{i}].fees', 0))
+        comcap= float(form_data.get(f'investors[{i}].comcap', 0))
+           
+        # Create the investor object
+        investors.append(Investor(i, name, gppercent, fees, comcap))
 
+    return investors
 
+def initalize_securities(form_data, total_securities, investors):
+    # initialize_securities takes the form data and the total number of securities and returns a list of security objects
+    
+    securities = []
+    
+    for i in range(total_securities+1):
+        # Obtain relevant data
+        holder = investors[i].name # Later have this be chosen by the user
+        type = form_data.get(f'securities[{i}].type', "com")
+        comshares = float(form_data.get(f'securities[{i}].comshares', 0))
+        app = float(form_data.get(f'securities[{i}].app', 0))
+        liqpref = float(form_data.get(f'securities[{i}].liqpref', 0))
+        ranking = i # Later have this be chosen by the user
 
-def calculate_waterfall(InvestorSecurities, FounderShares, company_val):
-    # Initialize the waterfall
-    waterfall = []
+        # Create the security object
+        securities.append(Security(i,holder, type, comshares, app, liqpref, ranking))
+    
+    return securities
 
-    # Obtain the preferred securities in InvestorSecurities
-    pref_securities = [sec.preferred for sec in InvestorSecurities if sec.preferred is not None]
-    # Sort the pref_securities by their ranking from highest to lowest
-    pref_securities.sort(key=lambda x: x.ranking, reverse=True)
+# Need to revise this in accordance with Zandberg's conversion calculation
+def calculate_waterfall(investors, securities, company_val):
 
-    # Obtain the common securities in InvestorSecuritie
-    com_securities = [sec.common for sec in InvestorSecurities if sec.common is not None]
-    # Append the founder common shares to the common securities
-    com_securities.append(FounderShares)
+    # Waterfall initialized so that columns are company value then cumdist for each security
+    # first row contains the secID of the security in that column, second row is all 0 (start of waterfall)
+    waterfall = np.zeros((2,len(securities)+1))
+    for i in range(len(securities)):
+        waterfall[0][i+1] = securities[i].secID
+   
+    # Start with redeeming preferred
+    preferred = []
+    # obtain the preferred securities and associated secID, redemption val and rankings
+    for sec in securities:
+        if sec.redeemval > 0:
+             pref = [sec.secID, sec.redeemval, sec.ranking]
+             preferred.append(pref)
 
-    # Calculate the total app * liqpref for all preferred securities
-    total_pref_val = sum([sec.app * sec.liqpref for sec in pref_securities])
+    # Generate dictionary of each ranking and the sum of redeemval for that ranking, plus total redeemval
+    rankings_dict = {}
+    total_rv = 0
+    for pref in preferred:
+        ranking = pref[2]
+        redeemval = pref[1]
+        if ranking in rankings_dict:
+            rankings_dict[ranking] += redeemval
+        else:
+            rankings_dict[ranking] = redeemval 
+        total_rv += redeemval
 
-    #Initialize the distributable amount
-    total_distributable = company_val
+    # populate waterfall for each distinct preferred ranking
+    for ranking in sorted(rankings_dict.keys(), reverse=True):
+        new_row = np.copy(waterfall[-1, :])
+        new_row[0] += rankings_dict[ranking]
+        for sec in securities:
+            if sec.ranking == ranking:
+                new_row[sec.secID+1] += sec.redeemval
+        waterfall = np.vstack([waterfall, new_row])
 
-    # Distribute the company_val among the preferred securities
-    for sec in pref_securities:
-        # Distribute to each preferred security the app*liqpref up to distributable amount
-        sec.distributed=min(sec.app * sec.liqpref, max(total_distributable,0))
-        total_distributable -= sec.distributed
-        waterfall.append([sec.holder,sec.ranking,sec.distributed])
-
-    # Distribute the remaining amount to the common securities
-    if (total_pref_val<company_val):
-        # For each common associated with a conv_pref, calculate the comppany value at which it converts
+    # Sum the total active common shares
+    total_active_common = 0
+    com_to_convert = []
+    for sec in securities:
+        if sec.active == 1:
+            total_active_common += sec.comshares
+        else:
+            secID = sec.secID
+            rv = sec.redeemval
+            com = sec.comshares
+            rvps = sec.rvps
+            new_row = {'secID': secID, 'redeemval': rv, 'comshares': com, 'rvps': rvps, 'conv_value': ''}
+            com_to_convert.append(new_row)
+    
+    # Calculate conversion threshold for each convertible security
+    com_to_convert = sorted(com_to_convert, key=lambda x: x['rvps'])
+    convertedRV = 0
+    dummy_active_common = total_active_common
+    for sec in com_to_convert:
+        conv_value = sec['redeemval']*((dummy_active_common + sec['comshares'])/sec['comshares'])+(total_rv - convertedRV - sec['redeemval'])
+        sec['conv_value']=conv_value
+        dummy_active_common += sec['comshares']
+        convertedRV += sec['redeemval']
         
-        
+
+    # Distribute value until all inactive common shares are converted
+    for conv_pref in com_to_convert:
+        # Obtain the last waterfall row with cum dist values
+        last_row = np.copy(waterfall[-1, :])
+        # Copy for new row
+        new_row = last_row.copy()
+        # Set the company value to the first conversion threshold
+        new_row[0] = conv_pref['conv_value']
+        # Calculate the value distributable in this segment
+        value_dist = new_row[0] - last_row[0]
+        for sec in securities:
+            if sec.dumactive == 1:
+                new_row[sec.secID+1] += value_dist * (sec.comshares/total_active_common)
+            else:
+                if sec.secID == conv_pref['secID']:
+                    sec.dumactive = 1
+                    total_active_common += sec.comshares
+        waterfall = np.vstack([waterfall, new_row])
+
+    # Add 20% to last entry to see continuation if company value is smaller than last conversion threshold
+    last_row = np.copy(waterfall[-1, :])
+    new_row = last_row.copy()
+    # Set the company value to the first conversion threshold
+    if new_row[0] > company_val:
+        new_row[0] = last_row[0]*1.2
+    else:
+        new_row[0]=company_val    
+    value_dist = new_row[0] - last_row[0]
+    for sec in securities:
+        if sec.dumactive == 1:
+            new_row[sec.secID+1] += value_dist * (sec.comshares/total_active_common)
+    waterfall = np.vstack([waterfall, new_row])
+
     return waterfall
+
+
+def create_waterfall_chart(waterfall, securities, timestamp):
+    # Assume the first column is the x axis and the other columns are line series
+    x = waterfall[:, 0]
+    y_list = [waterfall[:, i] for i in range(1, waterfall.shape[1])]
+
+#    Set the figure size and font properties
+    plt.figure(figsize=(7, 3))
+    plt.rcParams.update({'font.size': 8, 'font.family': 'serif'})
+    plt.subplots_adjust(bottom=0.2)
+
+    # Plot each line series with a different color
+    num_colors = len(securities)  
+    color_map = cm.get_cmap('tab20', num_colors)  # choose a colormap and number of colors
+    colors = color_map.colors  # get a list of colors
+    for i, y in enumerate(y_list):
+        plt.plot(x, y, color=colors[i], label='{}, {}'.format(securities[i].type_name, securities[i].holder))
+
+    # Set the x and y axis labels and title
+    plt.xlabel('Company Valuation')
+    plt.ylabel('Cumulutive Distibution to Security')
+    plt.title('Waterfall: All Securities')
+
+    # Add a legend to the chart
+    plt.legend(loc='upper left')
+
+    # Save the chart as a png file
+    filename = f"static/charts/wf_all_{timestamp}.png"  # Adds timestamp to filename
+    plt.savefig(filename)
+
 
 
 def has_attribute(obj, attr):
@@ -99,38 +222,27 @@ def submit():
     # Obtain the form data as a dictionary
     form_data = request.form.to_dict()
 
-    # Initialize the founder and founder shares
-    Founder = Investor(investorID="Founder")
-    founder_com_shares = float(form_data.get('founder_common_shares',0))
-    FounderShares   = Common(investorID=Founder.investorID, shares=founder_com_shares)
+    # Set total investors and total securities (counting from 0)
+    total_investors = 2 # Later have this be chosen by the user
+    total_securities = 2 # Later have this be chosen by the user
 
-    # Initialize the VC investor
-    VCInvestor = Investor(investorID="VC Investor")
+    # Initialize the investors and securities
+    investors = initialize_investors(form_data, total_investors)
+    securities = initalize_securities(form_data, total_securities, investors)
 
-    # Obtain Series A security data
-    serA_sec_type = form_data.get('serA_sec_type',None)
-    serA_app = float(form_data.get('serA_app',0))
-    serA_liqpref = float(form_data.get('serA_liqpref',0))
-    serA_shares = float(form_data.get('serA_shares',0))
-
-    # Initialize the Series A security
-    seriesA_pref = Preferred(investorID=VCInvestor.investorID, ranking=1, app=serA_app, liqpref=serA_liqpref)
-    if serA_sec_type == "red_pref":
-        seriesA_com = None
-    else:
-        seriesA_com = Common(investorID=VCInvestor.investorID, shares=serA_shares)
-        if serA_sec_type == "conv_pref":
-            seriesA_com.active = 0
-    SeriesA = InvestorSecurity(investorID=VCInvestor.investorID, common=seriesA_com, preferred=seriesA_pref, sec_type=serA_sec_type)
-    
+    # Obtain the company valuation
     company_val = float(form_data.get('company_val',1000))
 
-    InvestorSecurities= [SeriesA]
-    waterfall = calculate_waterfall(InvestorSecurities, FounderShares, company_val)
+    # Calculation the waterfall
+    waterfall = calculate_waterfall(investors, securities, company_val)
 
-    return render_template('response.html', form=request.form, Founder=Founder, VCInvestor=VCInvestor, FounderShares=FounderShares, SeriesA=SeriesA,waterfall=waterfall)
+    # Create unique timestamp for the images
+    timestamp = int(time.time())
 
+    # Create the waterfall chart
+    create_waterfall_chart(waterfall, securities, timestamp)
 
+    return render_template('response.html', investors=investors, securities=securities, company_val = company_val, waterfall=waterfall,ts=timestamp)
 
 
 
